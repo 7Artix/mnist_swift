c9878e8aede55a772cbce8128e7cd28d2140bc4f
Artix <artixzhang@icloud.com>
2024-12-08 18:40:48 +0800

    Moved some node function to the class: Node

--- Sources/nn_basis.swift
+++ Sources/nn_basis.swift
@@ -21,23 +21,3 @@ func generateNormal(mean: Double, stdDev: Double) -> Double {
     let z = sqrt(-2.0 * log(u1)) * cos(2.0 * .pi * u2)
     return mean + z * stdDev
 }
-
-//MARK: 初始化函数
-//Glorot初始化 for weight
-func glorotInitializer(inputSize: Int, outputSize: Int?) -> Double {
-    let limit = sqrt(6.0 / Double(inputSize + (outputSize ?? 0)))
-    return Double.random(in: -limit...limit)
-}
-//HE初始化 for weight
-func heInitializer(inputSize: Int, _: Int?) -> Double {
-    let stdDev = sqrt(2.0 / Double(inputSize))
-    return generateNormal(mean: 0.0, stdDev: stdDev)
-}
-//常值初始化 for weight
-func constantInitializer(_: Int, _: Int?) -> Double {
-    return 0.5
-}
-//bias初始化
-func biasInitializer(value: Double) -> Double {
-    return value
-}
